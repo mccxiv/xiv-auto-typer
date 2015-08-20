a1e66171cb165ef0f16d81f2c2e3d30116a36e5f
Andrea Stella <adr.stella@gmail.com>
2015-08-20 14:39:21 -0600

    Add node side logic

--- src/main.js
+++ src/main.js
@@ -1,43 +1,43 @@
+//=========================================================
+// Variables
+//=========================================================
+
 var app = require('app');
 var ipc = require('ipc');
 var robot = require('robotjs');
 var Window = require('browser-window');
+var EventEmitter = require('events').EventEmitter;
 var globalShortcut = require('global-shortcut');
 var mainWindow;
-var controlScheme = 1;
-var shortcuts = [
-	['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8'],
-	['CmdOrCtrl+F1', 'CmdOrCtrl+F2', 'CmdOrCtrl+F3', 'CmdOrCtrl+F4', 'CmdOrCtrl+F5', 'CmdOrCtrl+F6', 'CmdOrCtrl+F7', 'CmdOrCtrl+F8']
-];
 
-app.on('ready', load);
-app.on('ready', registerShortcuts);
+var shortcuts = {
+	regular: ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8'],
+	control: ['CmdOrCtrl+F1', 'CmdOrCtrl+F2', 'CmdOrCtrl+F3', 'CmdOrCtrl+F4', 'CmdOrCtrl+F5', 'CmdOrCtrl+F6', 'CmdOrCtrl+F7', 'CmdOrCtrl+F8'],
+	alt: ['Alt+F1', 'Alt+F2', 'Alt+F3', 'Alt+F4', 'Alt+F5', 'Alt+F6', 'Alt+F7', 'Alt+F8']
+};
+
+var settings = {
+	inputs: [],
+	after: 'none',
+	scheme: 'regular'
+};
+
+//=========================================================
+// Setup
+//=========================================================
+
+app.on('ready', makeWindow);
 app.on('window-all-closed', close);
 
+ipc.on('input-change', inputChanged);
+ipc.on('after-change', afterChanged);
+ipc.on('scheme-change', schemeChanged);
 
-function registerShortcuts() {
-	shortcuts[controlScheme].forEach(function(shortcut, index) {
-		globalShortcut.register(shortcut, function() {
-			getString(index, function(string) {
-				type(string);
-			});
-		})
-	});
-}
+//=========================================================
+// Main functions
+//=========================================================
 
-/**
- * Asks client window for the user's string at a certain index
- * @param index
- */
-function getString(index, cb) {
-	ipc.once('string', function(event, arg) {
-		console.log(arg);
-		cb(arg);
-	});
-	mainWindow.send('string?', index);
-}
-
-function load() {
+function makeWindow() {
 	var windowOpts = {
 		'use-content-size': true,
 		resizable: true,
@@ -54,12 +54,72 @@ function load() {
 	mainWindow.on('closed', function() {
 		mainWindow = null;
 	});
+
+	requestSettingsFromWindow();
 }
 
 function close() {
 	app.quit();
 }
 
-function type(string) {
-	robot.typeString(string);
+/**
+ * Handler for the ipc input-change event
+ * @param {{index: number, value: string}} arg
+ */
+function inputChanged(arg) {
+	var value = arg.value;
+	var shortcut = shortcuts[settings.scheme][arg.index];
+
+	settings.inputs[arg.index] = value;
+	unregister(shortcut);
+
+	if (value) register(shortcut, typeFunction(value));
+}
+
+function afterChanged(after) {
+	settings.after = after;
+}
+
+function schemeChanged(scheme) {
+	var oldScheme = settings.scheme;
+	settings.scheme = scheme;
+	unregisterAll(oldScheme);
+	registerAll();
+}
+
+//=========================================================
+// Helper functions
+//=========================================================
+
+function requestSettingsFromWindow() {
+	mainWindow.send('settings?');
+	mainWindow.once('settings', function(setts) {
+		settings = setts;
+	});
+}
+
+function unregisterAll(scheme) {
+	shortcuts[scheme].map(unregister);
+}
+
+function registerAll() {
+	settings[scheme].forEach(function(accelerator, index) {
+		if (settings.inputs[index]) register(accelerator, typeFunction(settings.inputs[index]));
+	})
+}
+
+function unregister(accelerator) {
+	if (globalShortcut.isRegistered(accelerator)) globalShortcut.unregister(accelerator);
+}
+
+function register(accelerator, fn) {
+	globalShortcut.register(accelerator, fn);
+}
+
+function typeFunction(string) {
+	return function() {
+		robot.typeString(string);
+		if (settings.after === 'enter') robot.keyTap('enter');
+		if (settings.after === 'tab') robot.keyTap('tab');
+	};
 }
